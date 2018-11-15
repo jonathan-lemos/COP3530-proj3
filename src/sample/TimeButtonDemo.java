b7812d0dd1d775822f99b39956e766f612b29bdc
Jonathan Lemos <jonathanlemos@protonmail.com>
2018-11-15 12:14:27 -0500

    added more comments. fixed missing eu picture

--- src/sample/TimeButtonDemo.java
+++ src/sample/TimeButtonDemo.java
@@ -27,32 +27,74 @@ public class TimeButtonDemo extends Application {
      * Because i know people will be looking at this source, I will try to explain it to the best of my ability.
      *
      * Basically JavaFX has a sort of hierarchy like this:
-     *                      Stage
-      *                       |
-      *                     Scene
-      *                       |
-      *                    Root Node
-      *                   /         \
-      *                Parent      Parent
-      *                Node 1      Node 2 (can have more)
-      *               /     \      /     \
-      *            (child nodes) (child nodes)
      *
-     * Node : A single object (e.g. a button).
-     * Pane : A container for nodes. The type of pane determines how the nodes are arranged within it.
-     * Scene: Groups all your panes together.
+     *                     Stage
+     *                       |
+     *                     Scene
+     *                       |
+     *                   Root Node
+     *                       |
+     *                  Child node(s)
+     *
+     * Stage: The current window (container) of the application.
+     * Scene: Container of the contents of the window. These contents are in the form of a root node.
+     * Node : An object that is displayed.
+     *        Note that nodes can contain other nodes (children).
+     *        The children of a node can be arranged into a graph called a "scene graph".
+     *        The scene graph for this application looks like this:
+     *
+     *                    pane(BorderPane)
+     *                   /               \
+     *          clock(DigitalClock)    paneForButtons(HBox)
+     *                                     /          \
+     *                              b12(Button)     b24(Button)
+     *                                   |               |
+     *                             i12(ImageView)  i24(ImageView)
+     *
+     * This is a data structure called a "tree", which makes this an easy structure for the computer to traverse.
+     * The root of the tree (pane) is called our "root node", and is the argument passed to our Scene.
+     * The Scene then draws our application based on the above scene graph.
+     * The drawing is done post-order, because the positioning of parents depends on their children.
      */
 
     protected BorderPane getPane() {
         // pane for containing buttons and clock
+
+        /*
+         * A BorderPane contains "regions" that can be assigned to.
+         * We are only interested in its center and bottom regions.
+         * This is how we keep our children aligned.
+         */
         BorderPane pane = new BorderPane();
 
         // pane for containing buttons
+
+        /*
+         * An HBox is a "horizontal box".
+         * It arranges its children horizontally, which is perfect for the button bar we are creating.
+         *
+         * The parameter (50) determines the space between each child of the HBox.
+         */
         HBox paneForButtons = new HBox(50);
+
+        /*
+         * We need to set a border for this HBox.
+         *
+         * This sets the HBox's border to a new Border with a green, solid stroke with right angles at its corners and the default width.
+         */
         paneForButtons.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 
         // clock to be added to pane
+
+        /*
+         * This instantiates a DigitalClock class as defined in the below class.
+         * This is essentially a label that updates its text every second with the current time.
+         */
         DigitalClock clock = new DigitalClock();
+
+        /*
+         * This causes our clock to be aligned in the center of our parent's region.
+         */
         clock.setAlignment(Pos.CENTER);
 
         // write code for buttons
@@ -61,7 +103,7 @@ public class TimeButtonDemo extends Application {
         i12.setFitWidth(40);
         Button b12 = new Button("12 hr", i12);
 
-        ImageView i24 = new ImageView(new Image(new File("usa.jpg").toURI().toString()));
+        ImageView i24 = new ImageView(new Image(new File("eu.jpg").toURI().toString()));
         i24.setFitHeight(25);
         i24.setFitWidth(40);
         Button b24 = new Button("24 hr", i24);
